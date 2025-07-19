9800d591d56a253a4b1b9a0bcd54d09180f0d0fc
Mohammad Ranjbar <mohammadranjbar.mmr81@gmail.com>
2025-07-19 18:26:14 +0330

    write testCrud

--- src/main/java/com/example/SpringDataJpa3/client/StudentClient.java
+++ src/main/java/com/example/SpringDataJpa3/client/StudentClient.java
@@ -7,19 +7,26 @@ import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
 import org.springframework.stereotype.Component;
 
-@Component
+//When a class like StudentClient is annotated with @Component
+//Spring automatically runs this class when the application (or test) is started because it is an ApplicationRunner.
+//The code you write inside run() is executed at the same time as the test.
+//@SpringBootTest loads the entire application context
+//When you use @SpringBootTest, Spring loads the entire application context, meaning:
+//All beans are loaded (including StudentClient)
+//If StudentClient is still active, its run() method is also executed, even before the tests
+//@Component
 public class StudentClient implements ApplicationRunner {
 
     private final StudentRepository studentRepository;
 
-    @Autowired
+    //@Autowired
     public StudentClient(StudentRepository studentRepository){
         this.studentRepository = studentRepository;
     }
 
     @Override
     public void run(ApplicationArguments args) {
-        Student student = new Student("Mohammad Ranjbar", "99111303");
+        Student student = new Student("Mohammad Ranjbar", "99111303"); // Transient state
         studentRepository.save(student);
     }
 
--- src/test/java/com/example/SpringDataJpa3/SpringDataJpa3ApplicationTests.java
+++ src/test/java/com/example/SpringDataJpa3/SpringDataJpa3ApplicationTests.java
@@ -1,13 +1,37 @@
 package com.example.SpringDataJpa3;
 
+import com.example.SpringDataJpa3.entity.Student;
+import com.example.SpringDataJpa3.repository.StudentRepository;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest // This annotation causes the entire Spring Boot application context to be loaded.
 class SpringDataJpa3ApplicationTests {
 
+	@Autowired
+	private StudentRepository studentRepository;
+
 	@Test
-	void contextLoads() {
+	public void testCrud(){
+		//Create
+		Student student = new Student("Mohamad Ranjbar", "991113031");
+		Student returnedStudent = studentRepository.save(student);
+		Assertions.assertNotNull(returnedStudent.getId());
+
+		//Update
+		returnedStudent.setName("Mohammad Ranjbar");
+		Student updatedStudent = studentRepository.save(returnedStudent);
+		Assertions.assertEquals("Mohammad Ranjbar", updatedStudent.getName());
+
+		//Read
+		Student foundStudent = studentRepository.findById(returnedStudent.getId()).get();
+		Assertions.assertEquals(1L, foundStudent.getId());
+
+		//Delete
+		studentRepository.delete(foundStudent);
+		Assertions.assertFalse(studentRepository.existsById(foundStudent.getId()));
 	}
 
 }
